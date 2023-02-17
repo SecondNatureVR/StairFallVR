1b9f52c34e8405184d1fb8f5db0613547e21b441
Johnny <secondnaturevr@gmail.com>
2023-02-16 20:03:22 -0500

    added play bounds

--- Assets/Scripts/PlayArea.cs
+++ Assets/Scripts/PlayArea.cs
@@ -8,15 +8,19 @@ public class PlayArea : MonoBehaviour
     private GameManager game;
     private OVRGrabber[] grabbers;
     private BoxCollider playArea;
+    private GameObject player;
+    private Vector3 spawnPoint;
 
     private void Start()
     {
         game = GameObject.FindWithTag("GameManager").GetComponent<GameManager>();
         grabbers = GameObject.FindGameObjectsWithTag("Grabbers").Select(g => g.GetComponent<OVRGrabber>()).ToArray();
         playArea = GetComponent<BoxCollider>();
+        player = GameObject.FindWithTag("Player");
+        spawnPoint = player.transform.position;
     }
 
-    public void Update()
+    public void LateUpdate()
     {
         foreach(var grabber in grabbers)
         {
@@ -25,6 +29,11 @@ public class PlayArea : MonoBehaviour
                 game.DisablePlayer();
                 gameObject.SetActive(false);
             }
+        }
+
+        if (!playArea.bounds.Contains(player.transform.position))
+        {
+            player.transform.position = spawnPoint;
         }
     }
 }
